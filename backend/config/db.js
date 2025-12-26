859671c3af85bfcdd8d8257e75c84b83633386aa
Om Kansal <omkansal285@gmail.com>
2025-12-26 09:52:16 +0530

    fix mongodb secret key

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://omkansal285:9761@cluster0.tvu4jki.mongodb.net/food-del').then(() => console.log("DB Connected"));
+    await mongoose.connect('---Mongodb URI---').then(() => console.log("DB Connected"));
 }
