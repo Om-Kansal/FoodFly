ca9d613850c1502860bc60a49b3837469d53e7d1
Om-Kansal <omkansal285@gmail.com>
2026-02-27 21:29:32 +0530

    Update frontend URL for order placement

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 const placeOrder = async (req, res) => {
 
     //const frontend_url = "https://food-fly-fln2.vercel.app/"
-    const frontend_url = "http://localhost:5173"
+    //const frontend_url = "http://localhost:5173"
+    const frontend_url = "https://foodfly-frontend.onrender.com/"
 
     try{
         const newOrder = new orderModel({
